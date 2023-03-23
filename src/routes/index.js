6bc9ae9823b5558d0d0ad0532ecb231aad182ca0
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2023-03-23 02:20:33 -0700

    modified adminAPi bug,need to consider Functions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
-const indexController = require('../controller')
-
+const indexController = require("../controller");
 
 router.get("/api/admin/users", indexController.getAllUsers);
-router.get("/api/admin/users/:id", indexController.getUserById);
+router.get("/api/admin/users/:_id", indexController.getUserById);
 router.post("/api/admin/create/user", indexController.createUser);
 
 router.get("/api/login/findbyemail", indexController.getOneByEmail);
@@ -12,9 +11,6 @@ router.get("/api/login/findbypassword", indexController.getOneByPassword);
 router.post("/api/admin/create/report", indexController.createReport);
 router.get("/api/admin/reports", indexController.getAllReports);
 
-
-router.get("/api/admin/reports/:id", indexController.getReportById);
-
-
+router.get("/api/admin/reports/:_id", indexController.getReportById);
 
 module.exports = router;
